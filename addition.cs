91df3a4de73b59a83acdddff579d84e2e1031d9e
Watchbleo <watchbleo@yahoo.fr>
2026-04-14 17:57:49 -0600

    addidng prime to the code

--- addition.cs
+++ addition.cs
@@ -28,5 +28,26 @@ public class HelloWorld
 
         int mod = num2%num1;
         Console.WriteLine ("division of 2 Numbers are: "+mod);
+
+        Console.WriteLine ("------------------------------------------");
+
+        Console.WriteLine("Prime Numbers between 1 and 100:");
+
+        for (int i = 2; i <= 100; i++)
+        {
+        bool isPrime = true;
+
+        for (int j = 2; j <= Math.Sqrt(i); j++)
+        {
+        if (i % j == 0)
+        {
+        isPrime = false;
+        break;
+        }
+        }
+
+        if (isPrime)
+        Console.Write(i + " ");
+        }
     }
 }
