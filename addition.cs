0409b1dc5562e438e824899772a715dd8c6cd037
Watchbleo <watchbleo@yahoo.fr>
2026-04-13 19:22:24 -0600

    my first code

---
+++ addition.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num1= 10;
+        int num2= 15;
+        int total=num1+num2;
+        Console.WriteLine (total);
+    }
+}
