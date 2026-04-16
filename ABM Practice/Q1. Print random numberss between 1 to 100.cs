40ed33f1f80a51f5801697626abf1187a48abd79
JozzeMC <jozze.jr@gmail.com>
2026-04-15 18:10:50 -0600

    Update Q1. Print random numberss between 1 to 100.cs

--- ABM Practice/Q1. Print random numberss between 1 to 100.cs
+++ ABM Practice/Q1. Print random numberss between 1 to 100.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+class Program
+{
+    static void Main()
+    {
+
+        List<int> numbers = Enumerable.Range(1, 100).ToList();
+        
+        Random rng = new Random();
+        int n = numbers.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+          
+            int value = numbers[k];
+            numbers[k] = numbers[n];
+            numbers[n] = value;
+        }
+        Console.WriteLine("Numbers from 1 to 100 in random order:");
+        foreach (int numb in numbers)
+        {
+            Console.Write(numb + " ");
+        }
+    }
+}
